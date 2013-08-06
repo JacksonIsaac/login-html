d543d75ff1f06d68645b78cc5cc44b95f0d33d28
Jackson Isaac <jacksonisaac2008@gmail.com>
2013-08-06 12:05:35 +0530

    duplicate window.open removed

--- login.js
+++ login.js
@@ -8,11 +8,7 @@ submit.addEventListener('click', function(){
 	console.log('out');
 	if(out == 'abc')  {
 		window.location.href = "http://www.google.com";
-<<<<<<< HEAD
 		window.open('http://www.google.com');
-=======
-		window.open('http://www.google.com');
->>>>>>> 6d066bb60c77c979c64d3c6febe1c7a3c734bb5b
 	}  
 	else
 	{
