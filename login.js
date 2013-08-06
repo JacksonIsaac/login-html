58849e23f63aa28c87d10679fb96e137d6ccf707
Jackson Isaac <jacksonisaac2008@gmail.com>
2013-08-06 11:45:09 +0530

    Initial commit

---
+++ login.js
@@ -0,0 +1,18 @@
+var user = document.getElementById('user');
+var pass = document.getElementById('pass');
+
+var submit = document.getElementById('submit');
+
+submit.addEventListener('click', function(){
+	var out = pass.value;
+	console.log('out');
+	if(out == 'abc')  {
+		window.location = "http://www.google.com";
+		//window.open('http://www.google.com');
+	}  
+	else
+	{
+		alert("Incorrect Password");
+	}
+    
+})
